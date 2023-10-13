af44320f5378d764d8da2390b1620127fb6e7e10
Pablo Mendoza <asdson5555@gmail.com>
2023-10-13 18:43:39 -0500

    rectangulo

---
+++ TareaTanaka07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaTanaka07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escoja la figura al calcular\n1. Rectángulo\n2. Triangulo\n3. Circulo");
+            int opt = int.Parse(Console.ReadLine());
+            switch (opt)
+            {
+                case 1: Console.WriteLine("Ingresa el valor de la base");
+                        float bas = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingresa el valor de la altura");
+                        float alt = int.Parse(Console.ReadLine());
+                        Rectángulo r = new Rectángulo(bas,alt);
+                    Console.WriteLine($"El área del rectángulo es {r.CalcularArea()}");
+                break;
+            }
+        }
+    }
+}
---
+++ TareaTanaka07/Rectángulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaTanaka07
+{
+    internal class Rectángulo
+    {
+        float b;
+        float h;
+        public Rectángulo(float b, float h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+        public float CalcularArea()
+        {
+            return b* h;
+        }
+    }
+}
