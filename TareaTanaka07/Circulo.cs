90d734cd7f0d5ae12aeb03e9155057276000b271
Pablo Mendoza <asdson5555@gmail.com>
2023-10-13 19:13:47 -0500

    circulo

---
+++ TareaTanaka07/Circulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaTanaka07
+{
+    internal class Circulo
+    {
+        float r;
+        public Circulo(float r)
+        {
+            this.r = r;
+        }
+        public float CalcularArea()
+        {
+            return (3.14f * (r * r)) / 2;
+        }
+    }
+}
--- TareaTanaka07/Program.cs
+++ TareaTanaka07/Program.cs
@@ -15,20 +15,29 @@ namespace TareaTanaka07
             switch (opt)
             {
                 case 1: Console.WriteLine("Ingresa el valor de la base");
-                        float basR = int.Parse(Console.ReadLine());
+                        float basR = float.Parse(Console.ReadLine());
                         Console.WriteLine("Ingresa el valor de la altura");
-                        float altR = int.Parse(Console.ReadLine());
+                        float altR = float.Parse(Console.ReadLine());
                         Rectángulo r = new Rectángulo(basR,altR);
                         Console.WriteLine($"El área del rectángulo es {r.CalcularArea()}");
+                        Console.ReadLine();
                     break;
 
                 case 2: Console.WriteLine("Ingresa el valor de la base");
-                        float basT = int.Parse(Console.ReadLine());
+                        float basT = float.Parse(Console.ReadLine());
                         Console.WriteLine("Ingresa el valor de la altura");
-                        float altT = int.Parse(Console.ReadLine());
+                        float altT = float.Parse(Console.ReadLine());
                         Triángulo t = new Triángulo(basT,altT);
                         Console.WriteLine($"El área del triángulo es {t.CalcularArea()}");
+                        Console.ReadLine();
                     break;
+                case 3: Console.WriteLine("Inserta el valor del radio");
+                        float rad = float.Parse(Console.ReadLine());
+                        Circulo C = new Circulo(rad);
+                        Console.WriteLine($"El área del triángulo es {C.CalcularArea()}");
+                        Console.ReadLine();
+                    break;
+
             }
         }
     }
